d05cdcc84f884a57ac3620faa3e9608097e7ba17
Dilpreet Kaur <dilpreetsuriofficial@gmail.com>
2023-09-14 01:53:19 +0530

    Modified WebDriverTest and Added relative locators

--- MyFirstTestngTestCase.java
+++ MyFirstTestngTestCase.java
@@ -1,13 +1,11 @@
 package scripts;
-//main method is not present in Test ng 
+
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-
 import static org.testng.Assert.assertEquals;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -17,7 +15,6 @@ import org.testng.annotations.AfterClass;
 public class MyFirstTestngTestCase {
 
 	WebDriver driver;
-//Methods which are having @Test annotation is your test Case
 
 	@Test
 	public void loginToNicheThyself() throws InterruptedException {
@@ -30,7 +27,6 @@ public class MyFirstTestngTestCase {
 		
           System.out.println(user.getAttribute("value"));
 		
-
 
 		WebElement password = driver.findElement(By.name("password"));
 		password.sendKeys("12345");
